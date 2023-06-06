7bc83bb6c3a1c16ba3219f0489707ee57eadd9ce
ikechukwu19 <bestielawhine@gmail.com>
2023-06-06 10:40:37 +0100

    text overflow

--- app/components/Messages.jsx
+++ app/components/Messages.jsx
@@ -6,10 +6,10 @@ const Messages = ({ messages, id }) => {
   return (
     <Link href={`/dashboard/${id}`}>
       <div className=" mb-4 flex h-16 w-full items-center justify-between rounded-xl border border-solid border-[#cacaca] px-3">
-        <p className=" overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+        <p className=" basis-[90%] overflow-hidden text-ellipsis whitespace-nowrap text-sm">
           {messages}
         </p>
-        <IoIosOpen className=" text-2xl" />
+        <IoIosOpen className="text-2xl" />
       </div>
     </Link>
   );
